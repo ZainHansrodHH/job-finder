4edd09497fa2ddcdf027dfefbc6bbfc633899b4a
Zain Hansrod <zainhansrod786@gmail.com>
2024-12-18 17:48:48 -0500

    Added logic for getting existing jobs and looping through our results

--- job-importer.php
+++ job-importer.php
@@ -13,13 +13,41 @@
 }
 
 function job_import() {
+
+    //Get the information from the api and store it if something is given otherwise log the error and return the function
+
     $api  = 'https://www.lifemark.ca/api/json/adp-jobs';
     $response = wp_remote_get($api);
 
-    if(is_wp_error($response)) {
-        error_log($response->get_error_message());
-        return;
-    }
+
+    //Decode the json response
+    $jobs = json_decode(wp_remote_retrieve_body($response), true);
+
+
+
+    //Basic wp query for getting the existing jobs
+    $existing_jobs = get_posts([
+        'post_type' => 'posts',
+        'numberposts' => -1,
+    ]);
+
+    $fetched_ids = [];
+
+    //Loop through the existing jobs and store the ids in the fetched_ids array
+    foreach($jobs as $job) {
+
+        //Limit the results to only the english ones
+        if($job['lang'] !== 'en_CA') {
+            continue;
+        }
+
+        $fetched_ids[] = $job['id'];
+
+        //Check if a post exists already
+        $existing_post_id = array_search($job['id'], $existing_jobs);
 
     
+    }
+
+
 }
