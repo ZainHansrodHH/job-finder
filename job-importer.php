7e9589bf71c7a6862773d48e872e0ccd0c1f72f2
Zain Hansrod <zainhansrod786@gmail.com>
2024-12-18 17:48:23 -0500

    Added basic structure for getting thr api

--- job-importer.php
+++ job-importer.php
@@ -7,3 +7,19 @@
  * Author: Zain Hansrod
  * Text Domain: job-importer
  */
+
+ if (!defined('ABSPATH')) {
+    exit; 
+}
+
+function job_import() {
+    $api  = 'https://www.lifemark.ca/api/json/adp-jobs';
+    $response = wp_remote_get($api);
+
+    if(is_wp_error($response)) {
+        error_log($response->get_error_message());
+        return;
+    }
+
+    
+}
