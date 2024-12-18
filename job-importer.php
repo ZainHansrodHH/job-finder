909cd6d35ccf665fac8e4a79746a63990418b8c8
Zain Hansrod <zainhansrod786@gmail.com>
2024-12-18 17:31:25 -0500

    Added main plugin file

---
+++ job-importer.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Job Importer
+ * Description: Imports jobs from a JSON feed as WordPress posts.
+ * Version: 1.0.0
+ * Author: Zain Hansrod
+ * Text Domain: job-importer
+ */
